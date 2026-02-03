dd229de1fda1b183678e99deb068540dcf2a9c73
Hassan <hachehri@gmail.com>
2026-02-04 00:22:48 +0100

    create logic php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
   <fieldset>
     <legend>Donnez votre sur php 8 !</legend>
-    <form action="#" method="post">
+    <form action="page.php" method="post">
         
         <input type="text" name="user_name" placeholder="Name">
         <br>
@@ -22,33 +22,19 @@
         <input type="email" name="email" placeholder="Email">
         <br>
         <br>
-        <textarea name="message" placeholder="Message"></textarea>
+        <textarea name="comments" placeholder="Comments"></textarea>
 
         <div class="btn_div">
-             <button>Envoyer</button>
-             <button>Afficher les avis</button>
+             <button type="submit" name="send">Envoyer</button>
+             <button type="submit" name="showResult">Afficher les avis</button>
         </div>
     </form>
   </fieldset>
+
+  
+
 
 
 </body>
 </html>
 
-<?php   
-   
-   $name=$_POST["user_name"];
-   $email=$_POST["email"];
-   $message=$_POST["message"];
-   
-
-   
-                               
-   $date= date("Y-m-d h-i");
-   
-
-   echo $new_lign = $name . " _ " . $email . " _ " . $message. " _ " . $date ;
-
-   file_put_contents("data.txt", $new_lign, FILE_APPEND);
-
-?>
--- page.php
+++ page.php
@@ -4,18 +4,46 @@
 
 <?php   
    
-   $name=$_POST["user_name"];
-   $email=$_POST["email"];
-   $message=$_POST["message"];
-   
+  if(isset($_POST["send"])){
 
+   $name=trim($_POST["user_name"]);
+   $email=trim($_POST["email"]);
+   $message=trim($_POST["comments"]);
    
-                               
+   if($name=="" || $email=="" || $message==""){
+        echo "Please write in this input";
+        return;
+   }
+
    $date= date("Y-m-d h-i");
    
 
-   echo $new_lign = $name . " _ " . $email . " _ " . $message. " _ " . $date ;
-
+   echo $new_lign = $name . " | " . $email . " | " . $message. " | " . $date ."\n";
+   
    file_put_contents("data.txt", $new_lign, FILE_APPEND);
+  }
+?>
+
+
+<?php  
+    
+    if(isset($_POST["showResult"])){
+   //  if (file_exists("data.txt")) {
+
+    $lines = file("data.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $lastFive = array_slice($lines, -5);
+
+    foreach ($lastFive as $line) {
+        list($n, $e, $m, $d) = explode(" | ", $line);
+
+        echo "<div class='review'>";
+        echo "<strong>$n</strong> ($e)<br>";
+        echo "$m<br>";
+        echo "<small>$d</small>";
+        echo "</div>";
+   //  }
+}
+    }
+
 
 ?>
