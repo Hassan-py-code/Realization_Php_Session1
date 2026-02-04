280309db551ebc40ed6abe53b315e8bd8bc993c2
Hassan <hachehri@gmail.com>
2026-02-04 16:29:53 +0100

    review in this code

--- page.php
+++ page.php
@@ -22,28 +22,36 @@
    
    file_put_contents("data.txt", $new_lign, FILE_APPEND);
   }
+
+
+
 ?>
+
 
 
 <?php  
     
-    if(isset($_POST["showResult"])){
+
+if(isset($_POST["showResult"])){
    //  if (file_exists("data.txt")) {
 
     $lines = file("data.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    
     $lastFive = array_slice($lines, -5);
-
+     
     foreach ($lastFive as $line) {
         list($n, $e, $m, $d) = explode(" | ", $line);
-
+       
         echo "<div class='review'>";
-        echo "<strong>$n</strong> ($e)<br>";
+        echo "<strong>$n</strong> ($e) <br>";
         echo "$m<br>";
         echo "<small>$d</small>";
         echo "</div>";
-   //  }
-}
-    }
 
+        
+    }
+}
+
+    // }
 
 ?>
